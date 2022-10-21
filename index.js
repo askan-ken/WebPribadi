ed628df2780250edd79e71c4b4589046fe0c3148
askan-ken <45626138+askan-ken@users.noreply.github.com>
2022-10-21 23:49:35 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+
+/*document.writeln("Hello")
+
+var firstname = 'Ardias'
+var midName = 'Askan'
+var lastName = 'Afisina'
+var fullName = 'Ardias Askan Afisina'
+var nickName = 'Ken'
+
+document.writeln(midName + ' ' + nickName)
+document.writeln("<br></br>")
+
+const ayang = 'Rizky Dwi Novita Putri'
+document.writeln(ayang)
+
+let hasil = 1 + 2;
+document.writeln("<p> 1 + 2 = " + hasil + "</p>")
+let hasilsebenarnya = hasil;
+
+hasil = hasil - 1;
+document.writeln("<p>" + hasilsebenarnya + " " + "- 1 = " + hasil + "</p>")
+hasilsebenarnya = hasil;
+
+hasil = hasil * 2;
+document.writeln("<p>" + hasilsebenarnya + " " + "x 2= " + hasil + "</p>")
+hasilsebenarnya = hasil;
+*/
+
+/*
+var firstname = 'Ardias'
+var midName = 'Askan'
+var lastName = 'Afisina'
+var fullName = 'Ardias Askan Afisina'
+var nickName = 'Ken'
+let usia = 21
+
+function genenrateBiodata() {
+    console.log("Nama saya adalah " + fullName + " saya biasa di panggil " + nickName + " umur saya adalah " + usia + " tahun")
+}
+
+genenrateBiodata()*/
+
+/*var firstname = 'Ardias'
+var midName = 'Askan'
+var lastName = 'Afisina'
+var fullName = 'Ardias Askan Afisina'
+var nickName = 'Ken'
+const waktu = new Date().getFullYear();
+
+function jam() {
+    if (waktu < 10) {
+        document.writeln("Masih pagi")
+    }
+    else if (waktu > 10){
+        document.writeln("Sudah siang")
+    }
+    else (waktu == 20){
+        document.writeln("Sudah malam")
+    }
+    console.log("Nama saya adalah " + fullName + " saya biasa di panggil " + nickName + " umur saya adalah " + usia + " tahun")
+}*/
